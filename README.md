4e2fe50d5609ce8b379eba419981fa5592b971e0
Noah Everett <noaheverett898@gmail.com>
2022-03-15 21:53:29 -0600

    Edits to CMakeLists.txt and README.md

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# Awake
-Application to keep mac awake while in clam shell mode (like Amphetamine)
+# **Awake**
+Application to keep mac awake while in clam shell mode (like [Amphetamine](https://www.google.com/search?q=amphetimine+app+store&rlz=1C5CHFA_enUS996US996&oq=amphetimine+app+store&aqs=chrome..69i57.3334j0j7&sourceid=chrome&ie=UTF-8#:~:text=Amphetamine%20on%20the,com%20%E2%80%BA%20app%20%E2%80%BA%20amphetamine)).
+
+## **Usage**
+To install Awake you will need [CMake](https://cmake.org/). I recommend installing CMake via [Homebrew](https://brew.sh/).
+
+Installing Homebrew: `/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"`
+
+Installing CMake: `brew install --cask cmake`
+
--- main.cpp
+++ main.cpp
@@ -14,20 +14,21 @@ int main( int argc, char** argv )
         return -1;
     }
 
-    string on = "echo " + password + " | sudo -S pmset -b sleep 0 2>nul; echo " + password + " | sudo -S pmset -b disablesleep 1 2>nul";
+    string on = "echo " + password + " | sudo -S pmset -b sleep 0 2>nul; echo " + password + " | sudo -S pmset -b disablesleep 0 2>nul";
     string off = "echo " + password + " | sudo -S pmset -b sleep 5 2>nul; echo " + password + " | sudo -S pmset -b disablesleep 0 2>nul";
     string unpluged;
-    cout << "Turning sleep off with command: sudo pmset -b sleep 0; sudo pmset -b disablesleep 1" << endl;
+    cout << "Turning sleep off with command: sudo pmset -b sleep 0; sudo pmset -b disablesleep 0" << endl;
 
     system( on.c_str() );
 
+    cout << "Do not terminate program" << endl;
     cout << "Unplug Mac then enter anything to continue: ";
     cin >> unpluged;
 
     cout << "Turning sleep off with command: sudo pmset -b sleep 5; sudo pmset -b disablesleep 0" << endl;
     system( off.c_str() );
 
-    cout << "Program complete. Bye bye!" << endl;
+    cout << "Program ran successfully. Bye bye!" << endl;
 
     return 0;
 }
