e4e089583542da3c0535c0d76b0b33d695c1a384
Noah Everett <noaheverett898@gmail.com>
2022-05-13 11:01:11 -0500

    Program will terminate itself when charging stops

--- main.cpp
+++ main.cpp
@@ -1,7 +1,33 @@
 #include <iostream>
+#include <stdexcept>
+#include <stdio.h>
 #include <string>
 
 using namespace std;
+
+string exec( string cmd ) {
+    char buffer[ 256 ];
+    string out = "";
+
+    FILE* pipe = popen( cmd.c_str(), "r" );
+    if( !pipe )
+        return "\0";
+
+    while( !feof( pipe ) )
+        if( fgets( buffer, 256, pipe ) != NULL )
+            out += buffer;
+
+    pclose( pipe );
+    return out;
+}
+
+bool isCharging( const string& password ) {
+    string out = exec( "echo " + password + "| sudo -S pmset -g ps 2>/dev/null" );
+    if( out.find( "discharging" ) != string::npos )
+        return false;
+
+    return true;
+}
 
 int main( int argc, char** argv )
 {
@@ -15,19 +41,15 @@ int main( int argc, char** argv )
 
     string password = getenv( "PASSWORD" );
 
-    string on = "echo " + password + " | sudo -S pmset -a sleep 0 2>/dev/null && sudo pmset -a hibernatemode 0 2>/dev/null && sudo pmset -a disablesleep 1 2>/dev/null";
-    string off = "echo " + password + " | sudo -S pmset -a sleep 1 2>/dev/null && sudo pmset -a hibernatemode 3 2>/dev/null && sudo pmset -a disablesleep 0 2>/dev/null";
-
     cout << "Turning sleep off with: sudo pmset -b sleep 0 && sudo pmset -a hibernatemode 0 && sudo pmset -a disablesleep 1" << endl;
-    system( on.c_str() );
+    string out = exec( "echo " + password + " | sudo -S pmset -a sleep 0 2>/dev/null && sudo pmset -a hibernatemode 0 2>/dev/null && sudo pmset -a disablesleep 1 2>/dev/null" );
 
-    cout << "Do not terminate program" << endl;
-    cout << "Unplug Mac then enter anything to continue: ";
-    string unpluged;
-    cin >> unpluged;
+    cout << "Program will terminate when computer stops sharging." << endl;
+    while( isCharging( password ) ) {}
+    cout << "Charging stopped." << endl;
 
     cout << "Turning sleep on with: sudo pmset -b sleep 1 && sudo pmset -a hibernatemode 3 && sudo pmset -a disablesleep 0" << endl;
-    system( off.c_str() );
+    exec( "echo " + password + " | sudo -S pmset -a sleep 1 2>/dev/null && sudo pmset -a hibernatemode 3 2>/dev/null && sudo pmset -a disablesleep 0 2>/dev/null" );
 
     cout << "Program ran successfully. Bye bye!" << endl;
 
